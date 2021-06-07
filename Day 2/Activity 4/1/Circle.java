231123a9f03be6bef63b97ea7c0d78706c19cceb
Vaibhav Gaur <VaibhavGaur56789@gmail.com>
2021-06-07 18:10:56 +0530

    Delete Day 2 directory

--- Day 2/Activity 4/1/Circle.java
+++
@@ -1,7 +0,0 @@
-class Circle extends Shape {
-	void calculateArea(int value) {
-		int area = (int)3.13 * value * value;
-		System.out.println("Area of circle is"+" "+area);
-	}
-
-}
--- Day 2/Activity 4/1/Square.java
+++
@@ -1,7 +0,0 @@
-class Square extends Shape {
-	void calculateArea(int value) {
-		int area = value * value;
-		System.out.println("Area of circle is"+" "+area);
-	}
-
-}
