bf52365546e0a50555913f8d157c34cf2c653bc9
Vaibhav Gaur <VaibhavGaur56789@gmail.com>
2021-06-21 00:00:44 +0530

    Add files via upload

---
+++ Day 5/Activity 11/Bowler.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Bowler
+{
+    private String name;
+
+    Bowler()
+    {
+        super();
+    }
+    Bowler(String name)
+    {
+        this.name=name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+}
